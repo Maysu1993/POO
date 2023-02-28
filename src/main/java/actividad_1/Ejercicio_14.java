eb5c59eb9afe95850aa7817a735a3f0708d19353
Maysu1993 <maylen.em2018.2018@gmail.com>
2023-02-27 19:38:09 -0500

    clase teclado, ejercicios 14 y 17

--- src/main/java/actividad_1/Ejercicio_14.java
+++ src/main/java/actividad_1/Ejercicio_14.java
@@ -1,26 +1,18 @@
 package actividad_1;
 
-import java.util.Scanner;
-
 public class Ejercicio_14 {
-    /**
-     * @param args
-     */
+
     public static void main(String[] args) {
         double numero;
-        try (Scanner leer_teclado = new Scanner(System.in)) {
-            System.out.println("Ingrese un numero para hallar su raiz cuadrada y cubica respectivamente: ");
-            numero= leer_teclado.nextDouble();
-        }
+
+        numero = Teclado.inputDouble("Ingrese un numero para hallar su raiz cuadrada y cubica respectivamente: ");
+
         double Cuadrado = Math.sqrt(numero);
         double Cubo = Math.cbrt(numero);
 
-        System.out.println("El cuadrado de  este numero es: " +Cuadrado);
+        System.out.println("El cuadrado de  este numero es: " + Cuadrado);
         System.out.println("El cubo de este numero es: " + Cubo);
 
-
-    
     }
 
-    
 }
--- src/main/java/actividad_1/Ejercicio_17.java
+++ src/main/java/actividad_1/Ejercicio_17.java
@@ -1,31 +1,17 @@
 package actividad_1;
 
-import java.util.Scanner;
-
 public class Ejercicio_17 {
     public static void main(String[] args) {
         double radio, areacirculo, longitud_circunferencias;
-        try (Scanner leer_teclado = new Scanner(System.in)) {
-            System.out.println("Ingrese el valor del radio: ");
-            radio = leer_teclado.nextDouble();
-        }
 
+        radio = Teclado.inputDouble("Ingrese el valor del radio: ");
 
-        areacirculo = Math.PI * Math.pow(radio,2);
+        areacirculo = Math.PI * Math.pow(radio, 2);
         longitud_circunferencias = 2 * Math.PI * radio;
-        
-        System.out.println("El area del circulo" + areacirculo);
-        System.out.println("La longitud de la circunferencia es:" + longitud_circunferencias);
 
+        System.out.println("El area del circulo: " + areacirculo);
+        System.out.println("La longitud de la circunferencia es: " + longitud_circunferencias);
 
-        
-
-
-
-
-        
-    
-    
     }
-    
+
 }
